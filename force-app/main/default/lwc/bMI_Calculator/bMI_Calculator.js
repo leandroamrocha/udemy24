1f997fad0d369ed147a49aa39dbff179179de3e4
Leandro Rocha <leandroamrocha@gmail.com>
2024-09-13 07:25:49 -0300

    sfdc init

---
+++ force-app/main/default/lwc/bMI_Calculator/bMI_Calculator.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class BMI_Calculator extends LightningElement {}
---
+++ force-app/main/default/lwc/readingMapFromApex/readingMapFromApex.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class ReadingMapFromApex extends LightningElement {}
